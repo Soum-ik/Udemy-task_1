a00f6225dd39e99ea0af7fce11211e8b9fb57596
Stack Soumik <110479389+Soum-ik@users.noreply.github.com>
2023-11-21 22:20:58 +0600

    push

--- vite-project/src/components/header.jsx
+++ vite-project/src/components/header.jsx
@@ -2,37 +2,58 @@ import { useState } from "react";
 import TagButton from "./TagButton";
 
 const Header = () => {
-  const [task, setTask] = useState("");
+  const [taskData, setTaskData] = useState({
+    task: "",
+    status: "todo",
+  });
+
+  console.log(taskData);
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setTaskData((prevTaskData) => ({
+      ...prevTaskData,
+      [name]: value,
+    }));
+  }
+
+  function handleSubmit() {
+    e.preventDefault()
+    console.log(taskData);
+  }
 
   return (
     <div className=" flex items-center flex-col justify-center pt-10 pb-10 ">
-      <div className=" min-w-[600px] relative">
+      <form onSubmit={handleSubmit} className=" min-w-[600px] relative">
         <input
           className={` w-full bg-slate-100 rounded-sm border outline-none focus:ring-1  focus:ring-offset-2 py-2 px-3 text-[15px] text-slate-600 font-medium`}
           type="text"
+          name="task"
           placeholder="Add your task"
-          value={task}
-          onChange={(e) => setTask(e.target.value)}
+          onChange={handleChange}
         />
         <div className=" grid grid-cols-3 gap-4 pt-5 ">
           <div className=" grid grid-flow-col gap-1 col-span-2 cursor-pointer">
-            <TagButton tagName={"HTML"}/>
-            <TagButton tagName={"CSS"}/>
-            <TagButton tagName={"JAVASCRIPT"}/>
-            <TagButton tagName={"REACT "}/>
+            <TagButton tagName={"HTML"} />
+            <TagButton tagName={"CSS"} />
+            <TagButton tagName={"JAVASCRIPT"} />
+            <TagButton tagName={"REACT"} />
           </div>
           <div className=" col-span-1 absolute  right-0 cursor-pointer">
-            <select className=" outline-none border py-2 px-3 rounded-md">
-              <option value="Doing">Doing</option>
-              <option value="Done">Done</option>
+            <select
+              onChange={handleChange}
+              name="status"
+              className=" outline-none border py-2 px-3 rounded-md"
+            >
               <option value="To Do">To Do</option>
+              <option value="Done">Done</option>
+              <option value="Doing">Doing</option>
             </select>
-            <button className=" border  rounded mx-2 py-2 px-3">
+            <button type="submit" className=" border  rounded mx-2 py-2 px-3">
               Add Task{" "}
             </button>
           </div>
         </div>
-      </div>
+      </form>
     </div>
   );
 };
--- vite-project/src/taskColume.jsx
+++ vite-project/src/taskColume.jsx
@@ -5,7 +5,7 @@ const TaskCoulumn = ({ tagName, icon }) => {
     <section>
       <h1 className=" pb-10 text-[28px] font-semibold flex items-center justify-center gap-2">
         {icon} {tagName}
-      </h1 >
+      </h1>
       <TaskCard />
     </section>
   );
